c03c4993e7fe4d3eba887c9cb8daee03746c41fb
YARD DEPARTAMENT <ivankochettov@gmail.com>
2022-09-12 20:18:11 +0300

    Add in test "main" branch

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
 # this is test branch
 
+print('Hello, World!')
+
+
+def func(a, b):
+    print('Сейчас сложатся два числа.')
+    result = a + b
+    return result
+
+def hui():
+    a = [i for i in range(0, 10)]
+    for j in range(len(a)):
+        print(a[j])
+
+hui()
+
+num1 = int(input('Введите первое число: '))
+num2 = int(input('Введите второе число: '))
+
+
+print(func(num1, num2))
+
 def func(a, b):
     print('Сейчас сложатся два числа.')
     result = a + b
