37b6da055a92a9e2821853edf524cc319ac9df62
YARD DEPARTAMENT <ivankochettov@gmail.com>
2022-09-12 19:41:19 +0300

    create

---
+++ main.py
@@ -0,0 +1,13 @@
+print('Hello, World!')
+
+
+def func(a, b):
+    print('Сейчас сложатся два числа.')
+    result = a + b
+    return result
+
+
+num1, num2 = 10, 20
+
+print(func(a=num1, b=num2))
+
