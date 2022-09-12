6830c71cd70fae42baa0a3f3c652ad9e75701233
YARD DEPARTAMENT <ivankochettov@gmail.com>
2022-09-12 20:17:14 +0300

    Add "write function" to file

--- main.py
+++ main.py
@@ -5,13 +5,22 @@ def func(a, b):
     result = a + b
     return result
 
+
 # this function reads file
-def test(file_dir):
+def read_file(file_dir):
     file = open(file_dir, 'r')
     a = file.read()
     for i in range(len(a)):
         print(a[i])
     file.close()
 
-test("text.txt")
+# this function dor write to file anything
+def write_file(file_dir, write_text):
+    file = open(file_dir, 'w')
+    file.write(write_text)
+    file.close()
 
+
+read_file("text.txt")
+write_file('text.txt', '\n\nХУЙ)')
+read_file('text.txt')
